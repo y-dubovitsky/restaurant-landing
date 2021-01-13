8772ebbaf7d02281462bbf22e499938557f8ee2f
User <y.dubovitsky@gmail.com>
2021-01-13 13:02:41 +0300

    Added static elements.

---
+++ src/test/java/space/dubovitsky/restaurantlanding/RestaurantlandingApplicationTests.java
@@ -0,0 +1,13 @@
+package space.dubovitsky.restaurantlanding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantlandingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
